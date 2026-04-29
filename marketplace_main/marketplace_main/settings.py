bcb44579676022be829965bc41a053c417c4667f
santyxhavez <santyxhavez37@gmail.com>
2026-04-28 17:26:18 -0700

    hicimos un nueva cosa llamada store

--- marketplace_main/marketplace_main/settings.py
+++ marketplace_main/marketplace_main/settings.py
@@ -37,7 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'store',
 ]
+AUTH_USER_MODEL = 'store.User'
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
