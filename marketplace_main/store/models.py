f65cbef4e8eab051d83f6fd0b25f51969670b744
santyxhavez <santyxhavez37@gmail.com>
2026-04-30 17:27:20 -0700

    cambio en los archivos y agregamos templates

--- marketplace_main/store/models.py
+++ marketplace_main/store/models.py
@@ -34,6 +34,7 @@ class Category(models.Model):
 class Product(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     name = models.CharField(max_length=150)
+    image = models.CharField(max_length=2000)
     description = models.TextField()
     price = models.DecimalField(max_digits=10, decimal_places=2)
     stock = models.PositiveIntegerField(default=0)
